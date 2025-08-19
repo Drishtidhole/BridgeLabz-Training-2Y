da62485ceeb0154a36dbc5e302f372290f1e6d59
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-08-19 15:37:05 +0530

    age_of_harry.java addition

---
+++ javaprogamming_level1/age_of_harry.java
@@ -0,0 +1,8 @@
+public class age_of_harry{
+    public static void main(String[] args) {
+        int birthyear = 2000;
+        int currentyear = 2024;
+        int age = currentyear-birthyear;
+        System.out.println("age of harry: " + age);
+    }
+}
