8c06f8bb25736e777721986f62191f6a2b826c17
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-08-19 21:48:25 +0530

    profitloss.java addition

---
+++ javaprogamming_level1/profitloss.java
@@ -0,0 +1,12 @@
+public class profitloss {
+    public static void main(String[] args) {
+        int cp= 129;
+        int sp= 191;
+        int profit = sp-cp;
+        int profit_percentage= profit / (cp*100);
+        System.out.printf("profit", profit,("profit percentage"), profit_percentage);
+
+
+    }
+    
+}
