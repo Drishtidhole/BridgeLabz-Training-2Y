49efd7f417f94f368d117ef78da4d9a5f6ab20ea
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-08-19 12:08:05 +0530

    areaofcircle.java addition

--- areaofcircle.java
+++ areaofcircle.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 public class areaofcircle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner (System.in);
         System.out.print("Enter the radius of the circle: ");
-        double radius = scanner.nextDouble();
+        double radius = sc.nextDouble();
         double area = Math.PI * radius * radius;
         System.out.printf("The area of the circle is: %.2f%n", area);
-        scanner.close();
+        
     }
 }
