722cba5f812cec8746bf1fc2b78a866adc0d02b6
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-08-19 23:19:25 +0530

    Squaresidecalculator.java addition

--- javaprogamming_level1/Squaresidecalculator.java
+++ javaprogamming_level1/Squaresidecalculator.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Squaresidecalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the side length of the square: ");
-        double side = scanner.nextDouble();
-        double area = side * side;
+        
+        System.out.print("Enter the length of one side of the square: ");
+        double sideLength = scanner.nextDouble();
+        
+        double area = sideLength * sideLength;
+        double perimeter = 4 * sideLength;
+        
         System.out.println("The area of the square is: " + area);
+        System.out.println("The perimeter of the square is: " + perimeter);
+        
         scanner.close();
     }
 }
