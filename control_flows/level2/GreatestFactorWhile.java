1d614325cc4d53622770453d5323c0e1161658fe
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-08-25 16:00:11 +0530

    push

---
+++ control_flows/level2/GreatestFactorWhile.java
@@ -0,0 +1,33 @@
+package control_flows.level2;
+
+import java.util.Scanner;
+
+public class GreatestFactorWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+       
+        int greatestFactor = 1;
+
+       
+        int i = number - 1;
+
+        
+        while (i >= 1) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+            i--;
+        }
+
+        
+        System.out.println("The greatest factor of " + number + " (besides itself) is: " + greatestFactor);
+
+        sc.close();
+    }
+}
