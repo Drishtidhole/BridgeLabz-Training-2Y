fa33f56ff4fa7430088914d0b9d14743fb4cc089
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-08-30 11:08:31 +0530

    push

---
+++ arrays_level2/largestsecondlargest.java
@@ -0,0 +1,57 @@
+package arrays_level2;
+
+import java.util.Scanner;
+
+public class largestsecondlargest {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+  
+        int maxDigit = 10; 
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        
+        while (number != 0) {
+            int digit = number % 10;  
+            digits[index] = digit;   
+            index++;
+
+        
+            if (index == maxDigit) {
+                break;
+            }
+            number = number / 10;  
+        }
+
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int i = 0; i < index; i++) {
+            int d = digits[i];
+            if (d > largest) {
+                secondLargest = largest;
+                largest = d;
+            } else if (d > secondLargest && d != largest) {
+                secondLargest = d;
+            }
+        }
+
+        
+        System.out.println("Largest digit = " + largest);
+        if (secondLargest == -1) {
+            System.out.println("Second largest digit does not exist.");
+        } else {
+            System.out.println("Second largest digit = " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
+
+
