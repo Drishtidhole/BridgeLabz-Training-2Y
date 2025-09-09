b55b5ef3c4577ce0ec4b0ecc4a056815a3d8099b
Drishtidhole <drishti.dhole_cs24@gla.ac.in>
2025-09-09 11:42:29 +0530

    Delete javaprogamming_level1 directory

--- javaprogamming_level1/age_of_harry.java
+++
@@ -1,8 +0,0 @@
-public class age_of_harry{
-    public static void main(String[] args) {
-        int birthyear = 2000;
-        int currentyear = 2024;
-        int age = currentyear-birthyear;
-        System.out.println("age of harry: " + age);
-    }
-}
--- javaprogamming_level1/kilometrestomiles.java
+++
@@ -1,9 +0,0 @@
-public class kilometrestomiles{
-    public static void main(String[] args) {
-        double kilometer= 10.8;
-        double miles = kilometer * 0.621371;
-        System.out.println("distance in miles: " + miles);
-    }
-}
-    
-
